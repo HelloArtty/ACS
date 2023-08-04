973845f3de70d8053d640105e37a3bae67f928f5
HelloArtty <chaithawat.sakl@kmutt.ac.th>
2023-08-04 15:15:16 +0700

    wrokshop7_new

--- python/Workshop7.py
+++ python/Workshop7.py
@@ -1,19 +1,38 @@
-#1
-def draw_diamond(x):
-    for i in range(x):
-        if i <= x//2:
-            print(' '*(x//2-i)+'#'*(2*i+1))
-        else:
-            print(' '*(i-x//2)+'#'*(2*(x-i)-1))
-    return ''
-print(draw_diamond(7))
+# #1
+# def draw_diamond(x):
+#     for i in range(x):
+#         if i <= x//2:
+#             print(' '*(x//2-i)+'#'*(2*i+1))
+#         else:
+#             print(' '*(i-x//2)+'#'*(2*(x-i)-1))
+#     return ''
+# print(draw_diamond(7))
 
 
 
 
 
-#2
-def draw_diamond(x):
-    return '\n'.join([' '*(x//2-i)+'#'*(2*i+1) if i <= x//2 else ' '*(i-x//2)+'#'*(2*(x-i)-1) for i in range(x)])
+# #2
+# def draw_diamond(x):
+#     return '\n'.join([' '*(x//2-i)+'#'*(2*i+1) if i <= x//2 else ' '*(i-x//2)+'#'*(2*(x-i)-1) for i in range(x)])
 
-print(draw_diamond(9))
+# print(draw_diamond(9))
+
+username = input("Enter your username: ")
+print("Hello,", username,". Let's crete your grocery list.")
+
+grocery_items = {}
+while True:
+    item = input("Enter the item(or 'done' to finish): ")
+    if item == 'done':
+        break
+    price = int(input("Enter the price of product : "))
+    grocery_items[item] = price
+
+print("Your Grocert List:")
+total_cost = 0
+for item, price in grocery_items.items():
+    print(f"{item}: {price}")
+    total_cost += price
+
+print("Total Cost: ", total_cost, "Baht")
